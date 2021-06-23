0bf157f4ba9dc91d4dadb546993463ae5bb734de
Mariana Aristizábal <maristizabal212@misena.edu.co>
2021-06-23 09:05:03 -0500

    js

--- js/codigo.js
+++ js/codigo.js
@@ -37,115 +37,80 @@ function addToCartClicked(event){
 }
 
 
-function addItemToShoppingCart(){
+// function addItemToShoppingCart(){
 
-    const shoppingCartContent = document.createElement("div");
-    shoppingCartContent.classList.add("container", "p-0", "mb-3", "col-12");
-    shoppingCartItemsContainer.appendChild(shoppingCartContent);
+//     const shoppingCartContent = document.createElement("div");
+//     shoppingCartContent.classList.add("container", "p-0", "mb-3", "col-12");
+//     shoppingCartItemsContainer.appendChild(shoppingCartContent);
 
-        const ulCartContent = document.createElement("ul");
-        ulCartContent.classList.add("list-group");
-        shoppingCartContent.appendChild(ulCartContent);
+//         const ulCartContent = document.createElement("ul");
+//         ulCartContent.classList.add("list-group");
+//         shoppingCartContent.appendChild(ulCartContent);
 
-            const liCartItem = document.createElement("li");
-            liCartItem.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center", "active");
-            ulCartContent.appendChild(liCartItem);
+//             const liCartItem = document.createElement("li");
+//             liCartItem.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center", "active");
+//             ulCartContent.appendChild(liCartItem);
 
 
-                const divCartItem = document.createElement("div");
-                divCartItem.classList.add("todo", "d-flex", "align-items-center", "justify-content-center", "color-primary-2", "font-ns", "col-11");
-                liCartItem.appendChild(divCartItem);
+//                 const divCartItem = document.createElement("div");
+//                 divCartItem.classList.add("todo", "d-flex", "align-items-center", "justify-content-center", "color-primary-2", "font-ns", "col-11");
+//                 liCartItem.appendChild(divCartItem);
 
-                    const contImgCartItem = document.createElement("h4");
-                    contImgCartItem.classList.add("col-3", "text-center");
-                    divCartItem.appendChild(contImgCartItem);
+//                     const contImgCartItem = document.createElement("h4");
+//                     contImgCartItem.classList.add("col-3", "text-center");
+//                     divCartItem.appendChild(contImgCartItem);
 
-                    const imgCartItem = document.createElement("img");
-                    imgCartItem.classList.add("car-img", "align-items-center");
-                    contImgCartItem.appendChild(imgCartItem);
+//                     const imgCartItem = document.createElement("img");
+//                     imgCartItem.classList.add("car-img", "align-items-center");
+//                     contImgCartItem.appendChild(imgCartItem);
 
-                    const titleCartItem = document.createElement("h4");
-                    titleCartItem.classList.add("text-center", "mb-0", "col-6");
-                    divCartItem.appendChild(titleCartItem);
+//                     const titleCartItem = document.createElement("h4");
+//                     titleCartItem.classList.add("text-center", "mb-0", "col-6");
+//                     divCartItem.appendChild(titleCartItem);
 
-                    const PriceCartItem = document.createElement("h4");
-                    PriceCartItem.classList.add("text-center", "mb-0", "col-3");
-                    divCartItem.appendChild(PriceCartItem);
+//                     const PriceCartItem = document.createElement("h4");
+//                     PriceCartItem.classList.add("text-center", "mb-0", "col-3");
+//                     divCartItem.appendChild(PriceCartItem);
 
-                const crossCartItem = document.createElement("div");
-                crossCartItem.classList.add("col-1", "d-flex", "align-items-center", "justify-content-center");
-                liCartItem.appendChild(crossCartItem);
+//                 const crossCartItem = document.createElement("div");
+//                 crossCartItem.classList.add("col-1", "d-flex", "align-items-center", "justify-content-center");
+//                 liCartItem.appendChild(crossCartItem);
 
-                    const crossImgCartItem = document.createElement("img");
-                    crossImgCartItem.classList.add("cross");
-                    crossCartItem.appendChild(crossImgCartItem);
+//                     const crossImgCartItem = document.createElement("img");
+//                     crossImgCartItem.classList.add("cross");
+//                     crossCartItem.appendChild(crossImgCartItem);
 
+// }
+
+
+function addItemToShoppingCart(cardTitle, cardPrice, cardImg){
+    const shoppingCartRow = document.createElement('div');
+    const shoppingCartContent = `
+        <div class="container p-0 mb-3 col-12">
+            <ul class="list-group" id="contProductos">
+                <li class="list-group-item d-flex justify-content-between align-items-center active">
+                    <div class="todo d-flex align-items-center justify-content-center color-primary-2 font-ns col-11">
+                        <h4 class="col-3 text-center">
+                            <img src=${cardImg} class="car-img align-items-center"> 
+                        </h4>
+                        <h4 class="text-center mb-0 col-6">${cardTitle}</h4>
+                        <h4 class="text-center mb-0 col-3">${cardPrice}</h4>
+                    </div>
+                    <div id="" class="col-1 d-flex align-items-center justify-content-center">
+                        <img  class="cross" src="images/iconos/x.png" alt="">
+                    </div>
+                </li>
+            </ul>
+        </div>`;
+    shoppingCartRow.innerHTML = shoppingCartContent;
+    shoppingCartItemsContainer.append(shoppingCartRow);
+
+    shoppingCartRow.querySelector('.cross').addEventListener('click', removeShoppingCartItem);
 }
 
-// function complete(e){
-    
-//     // Elimina el card al dar click en X
-//     if(e.target.classList.contains("cross")){    
-//         e.target.parentElement.remove();
-//         // itemsLeft.textContent = printItemsLeft();
-//         console.log();
-//     }else{
-//         // Añade clase cuando la tarea se complete
-//         if(e.target.checked == true){  
-//             e.target.parentElement.parentElement.classList.add("tarea-completada");
-//             // itemsLeft.textContent = printItemsLeft();
-//         }else{
-//             e.target.parentElement.parentElement.classList.remove("tarea-completada");
-//             // itemsLeft.textContent = printItemsLeft();
-//         }
-//     }
+shoppingCartRow.querySelector('.cross').addEventListener('click', removeShoppingCartItem);
 
-// }
-
-// function clearItems(e){
-//     const tareas = Array.from(contTareas.children);
-//     // console.log(tareas);
-
-//     let arrayCompleted = tareas.filter(function(tareaTarjeta){
-//         if(tareaTarjeta.classList.contains("tarea-completada")){
-//             return tareaTarjeta;
-//         }
-//     });
-
-//     arrayCompleted.forEach(function(element, i){
-//         arrayCompleted[i].remove();
-//     });
-
-// }
-
-
-// function addItemToShoppingCart(cardTitle, cardPrice, cardImg){
-//     const shoppingCartRow = document.createElement('div');
-//     const shoppingCartContent = `
-//         <div class="container p-0 mb-3 col-12">
-//             <ul class="list-group" id="contProductos">
-//                 <li class="list-group-item d-flex justify-content-between align-items-center active">
-//                     <div class="todo d-flex align-items-center justify-content-center color-primary-2 font-ns col-11">
-//                         <h4 class="col-3 text-center">
-//                             <img src=${cardImg} class="car-img align-items-center"> 
-//                         </h4>
-//                         <h4 class="text-center mb-0 col-6">${cardTitle}</h4>
-//                         <h4 class="text-center mb-0 col-3">${cardPrice}</h4>
-//                     </div>
-//                     <div id="" class="col-1 d-flex align-items-center justify-content-center">
-//                         <img  class="cross" src="images/iconos/x.png" alt="">
-//                     </div>
-//                 </li>
-//             </ul>
-//         </div>`;
-//     shoppingCartRow.innerHTML = shoppingCartContent;
-//     shoppingCartItemsContainer.append(shoppingCartRow);
-
-//         shoppingCartRow.querySelector('.cross').addEventListener('click', removeShoppingCartItem);
-// }
-
-
-// function removeShoppingCartItem(event){
-//     const buttonClicked = event.target;
-//     buttonClicked.closest('.shoppingCartItem').remove();
-// }
+function removeShoppingCartItem(event){
+    const buttonClicked = event.target;
+    buttonClicked.closest('.shoppingCartItem').remove();
+}
