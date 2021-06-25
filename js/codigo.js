cbd221df496d5ac1fe46920a45749718bfa3c165
Mariana Aristizábal <maristizabal212@misena.edu.co>
2021-06-24 20:05:05 -0500

    programación

--- js/codigo.js
+++ js/codigo.js
@@ -14,15 +14,21 @@ btnCambio.addEventListener("click", function(){
     }
 });
 
-
 // Variables
 const addToShoppingCartButtons = document.querySelectorAll('.c-add');
+const shoppingCartItemsContainer = document.querySelector('#shoppingContainer');
+const removeAllItems = document.querySelector('#clearAll');
 
+// Actualiza el número total de productos en carrito
+const totalCarrito = document.querySelector("#n-items");
+totalCarrito.textContent = totalProductos();
+
+// Eventos
 addToShoppingCartButtons.forEach(addToCartButton => {
     addToCartButton.addEventListener('click', addToCartClicked);
 });
-
-const shoppingCartItemsContainer = document.querySelector('#shoppingContainer');
+shoppingCartItemsContainer.addEventListener('click', removeShoppingCartItem);
+removeAllItems.addEventListener("click", clearItems);
 
 function addToCartClicked(event){
     const button = event.target;
@@ -31,57 +37,10 @@ function addToCartClicked(event){
     const cardTitle = card.querySelector('#title').textContent;
     const cardPrice = card.querySelector('#price').textContent;
     const cardImg = card.querySelector('.prod-img').src;
-    console.log('addToCartClicked -> cardPrice', cardTitle, cardPrice, cardImg);
+    // console.log('addToCartClicked -> cardPrice', cardTitle, cardPrice, cardImg);
 
     addItemToShoppingCart(cardTitle, cardPrice, cardImg);
 }
-
-
-// function addItemToShoppingCart(){
-
-//     const shoppingCartContent = document.createElement("div");
-//     shoppingCartContent.classList.add("container", "p-0", "mb-3", "col-12");
-//     shoppingCartItemsContainer.appendChild(shoppingCartContent);
-
-//         const ulCartContent = document.createElement("ul");
-//         ulCartContent.classList.add("list-group");
-//         shoppingCartContent.appendChild(ulCartContent);
-
-//             const liCartItem = document.createElement("li");
-//             liCartItem.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center", "active");
-//             ulCartContent.appendChild(liCartItem);
-
-
-//                 const divCartItem = document.createElement("div");
-//                 divCartItem.classList.add("todo", "d-flex", "align-items-center", "justify-content-center", "color-primary-2", "font-ns", "col-11");
-//                 liCartItem.appendChild(divCartItem);
-
-//                     const contImgCartItem = document.createElement("h4");
-//                     contImgCartItem.classList.add("col-3", "text-center");
-//                     divCartItem.appendChild(contImgCartItem);
-
-//                     const imgCartItem = document.createElement("img");
-//                     imgCartItem.classList.add("car-img", "align-items-center");
-//                     contImgCartItem.appendChild(imgCartItem);
-
-//                     const titleCartItem = document.createElement("h4");
-//                     titleCartItem.classList.add("text-center", "mb-0", "col-6");
-//                     divCartItem.appendChild(titleCartItem);
-
-//                     const PriceCartItem = document.createElement("h4");
-//                     PriceCartItem.classList.add("text-center", "mb-0", "col-3");
-//                     divCartItem.appendChild(PriceCartItem);
-
-//                 const crossCartItem = document.createElement("div");
-//                 crossCartItem.classList.add("col-1", "d-flex", "align-items-center", "justify-content-center");
-//                 liCartItem.appendChild(crossCartItem);
-
-//                     const crossImgCartItem = document.createElement("img");
-//                     crossImgCartItem.classList.add("cross");
-//                     crossCartItem.appendChild(crossImgCartItem);
-
-// }
-
 
 function addItemToShoppingCart(cardTitle, cardPrice, cardImg){
     const shoppingCartRow = document.createElement('div');
@@ -106,11 +65,84 @@ function addItemToShoppingCart(cardTitle, cardPrice, cardImg){
     shoppingCartItemsContainer.append(shoppingCartRow);
 
     shoppingCartRow.querySelector('.cross').addEventListener('click', removeShoppingCartItem);
-}
 
-shoppingCartRow.querySelector('.cross').addEventListener('click', removeShoppingCartItem);
+    totalCarrito.textContent = totalProductos();
+}
 
 function removeShoppingCartItem(event){
-    const buttonClicked = event.target;
-    buttonClicked.closest('.shoppingCartItem').remove();
+    if(event.target.classList.contains("cross")){    
+        event.target.parentElement.parentElement.parentElement.remove();
+        totalCarrito.textContent = totalProductos();
+        console.log();
+    }
 }
+
+function clearItems(e){
+    const productosCarrito = Array.from(shoppingCartItemsContainer.children);
+    console.log(productosCarrito);
+
+    let arrayCompleted = productosCarrito;
+
+    arrayCompleted.forEach(function(element, i){
+        arrayCompleted[i].remove();
+    });
+
+}
+
+function totalProductos(){
+
+    const productosCarrito = Array.from(shoppingCartItemsContainer.children);
+    console.log(productosCarrito);
+
+    let totalProductos = productosCarrito;
+
+    return totalProductos.length;
+
+}
+
+
+/*//FUNCIÓN CON CLASSLIST
+function addItemToShoppingCart(){
+
+    const shoppingCartContent = document.createElement("div");
+    shoppingCartContent.classList.add("container", "p-0", "mb-3", "col-12");
+    shoppingCartItemsContainer.appendChild(shoppingCartContent);
+
+        const ulCartContent = document.createElement("ul");
+        ulCartContent.classList.add("list-group");
+        shoppingCartContent.appendChild(ulCartContent);
+
+            const liCartItem = document.createElement("li");
+            liCartItem.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center", "active");
+            ulCartContent.appendChild(liCartItem);
+
+
+                const divCartItem = document.createElement("div");
+                divCartItem.classList.add("todo", "d-flex", "align-items-center", "justify-content-center", "color-primary-2", "font-ns", "col-11");
+                liCartItem.appendChild(divCartItem);
+
+                    const contImgCartItem = document.createElement("h4");
+                    contImgCartItem.classList.add("col-3", "text-center");
+                    divCartItem.appendChild(contImgCartItem);
+
+                    const imgCartItem = document.createElement("img");
+                    imgCartItem.classList.add("car-img", "align-items-center");
+                    contImgCartItem.appendChild(imgCartItem);
+
+                    const titleCartItem = document.createElement("h4");
+                    titleCartItem.classList.add("text-center", "mb-0", "col-6");
+                    divCartItem.appendChild(titleCartItem);
+
+                    const PriceCartItem = document.createElement("h4");
+                    PriceCartItem.classList.add("text-center", "mb-0", "col-3");
+                    divCartItem.appendChild(PriceCartItem);
+
+                const crossCartItem = document.createElement("div");
+                crossCartItem.classList.add("col-1", "d-flex", "align-items-center", "justify-content-center");
+                liCartItem.appendChild(crossCartItem);
+
+                    const crossImgCartItem = document.createElement("img");
+                    crossImgCartItem.classList.add("cross");
+                    crossCartItem.appendChild(crossImgCartItem);
+
+}*/
